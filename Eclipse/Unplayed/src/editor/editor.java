fb8c8a2a401192faaf1820d67bc267b28c42c1ea
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-06 21:08:37 +1300

    Added eclipse package to game data. Working in eclipse should make things a lot more efficent

---
+++ Eclipse/Unplayed/src/editor/editor.java
@@ -0,0 +1,13 @@
+package editor;
+
+import processing.core.*;
+//import static processing.core.PConstants.*;
+
+public class editor {
+	PApplet p;
+	
+	public editor(PApplet p) {
+		this.p = p;
+		//this is a test edit
+	}
+}
