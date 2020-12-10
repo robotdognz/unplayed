fa96ee049efe92bc1c7b824def5c64fd63097657
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-11 12:06:24 +1300

    implemented delete widget

--- Eclipse/Unplayed/src/editor/uiside/WidgetDelete.java
+++ Eclipse/Unplayed/src/editor/uiside/WidgetDelete.java
@@ -1,19 +1,61 @@
 package editor.uiside;
 
+import java.util.List;
+
 import editor.Editor;
 import editor.Toolbar;
+import game.Game;
+import objects.Event;
+import objects.Image;
+import objects.Page;
+import objects.Tile;
+import objects.View;
 import processing.core.PApplet;
 import ui.Widget;
 
 public class WidgetDelete extends Widget {
+	Game game;
+
 	public WidgetDelete(PApplet p, Editor editor, Toolbar parent) {
 		super(p, editor, parent);
-		icon = p.loadImage(folder + "confirmTick.png");
-		closeAfterSubWidget = true;
+		this.game = editor.game;
+		icon = p.loadImage(folder + "deleteButton.png");
 	}
 
 	public void clicked() {
-		
+		if (editor.selected != null) {
+			if (editor.selected instanceof Tile) {
+				game.world.remove(editor.selected);
+			} else if (editor.selected instanceof Image) {
+				game.world.remove(editor.selected);
+			} else if (editor.selected instanceof Event) {
+				game.world.remove(editor.selected);
+			} else if (editor.selected instanceof View) {
+				// remove matching the pages
+				List<Page> pages = game.getPageView().getPages();
+				for (int i = 0; i < pages.size(); i++) {
+					if (pages.get(i).getX() != editor.selected.getX()) {
+						continue;
+					}
+					if (pages.get(i).getY() != editor.selected.getY()) {
+						continue;
+					}
+					if (pages.get(i).getWidth() != editor.selected.getWidth()) {
+						continue;
+					}
+					if (pages.get(i).getHeight() != editor.selected.getHeight()) {
+						continue;
+					}
+					game.getPageView().removePage(pages.get(i));
+				}
+				// remove the view
+				game.views.remove(editor.selected);
+			} else if (editor.selected instanceof Page) {
+				game.getPageView().removePage((Page) editor.selected);
+			}
+			// deselect the object
+			editor.selected = null;
+		}
 	}
 
 	public void updateActiveUser() {
