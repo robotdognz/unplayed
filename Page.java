84e3db652406f927747afdf0c4dbcf789ab5c429
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-10 20:55:03 +1300

    pages now only redraw if the player is visible in them

--- Page.java
+++ Page.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import game.Game;
+import game.Player;
 
 import java.util.ArrayList;
 
@@ -14,14 +15,16 @@ import static processing.core.PConstants.*;
 public class Page extends Editable {
 	private PApplet p;
 	private Game game;
+	private Player player;
 	private Rectangle view; // the page's view into the world
 	private HashSet<Rectangle> pageObjects;
 	private HashSet<String> excludedObjects; // a list of rectangle strings to exclude while drawing
 
 	private PGraphics pageGraphics;
-	private int pageRedraw = 0;
-	private int redrawAmount = 60;
-	private PGraphics paperGraphics;
+	private boolean playerInside = true;
+//	private int pageRedraw = 0;
+//	private int redrawAmount = 60;
+//	private PGraphics paperGraphics;
 	private PVector position; // center of the page in page view
 	Rectangle adjustedRect; // an axis locked rectangle that contains the rotated page (used to check if
 							// page is on screen and therefore should be drawn)
@@ -30,6 +33,7 @@ public class Page extends Editable {
 		super(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y);
 		this.p = p;
 		this.game = game;
+		this.player = game.player;
 		float rWidth = bottomRight.x - topLeft.x;
 		float rHeight = bottomRight.y - topLeft.y;
 		this.view = new Rectangle(topLeft.x, topLeft.y, rWidth, rHeight);
@@ -37,14 +41,14 @@ public class Page extends Editable {
 		this.excludedObjects = new HashSet<String>();
 
 		this.pageGraphics = p.createGraphics((int) rWidth, (int) rHeight, P2D);
-		this.paperGraphics = p.createGraphics((int) rWidth, (int) rHeight, P2D);
-		
-		//draw paper
-		paperGraphics.beginDraw();
-		paperGraphics.translate(-view.getX(), -view.getY());
-		game.paper.draw(paperGraphics, view, 5 / size);
-		paperGraphics.endDraw();
-		
+		// this.paperGraphics = p.createGraphics((int) rWidth, (int) rHeight, P2D);
+
+		// draw paper
+//		paperGraphics.beginDraw();
+//		paperGraphics.translate(-view.getX(), -view.getY());
+//		game.paper.draw(paperGraphics, view, 1);
+//		paperGraphics.endDraw();
+
 		setPosition(position);
 	}
 
@@ -95,40 +99,42 @@ public class Page extends Editable {
 			}
 		}
 
-		// if(player is inside the page){ TODO: implement this
-		// begin drawing on the page
+		if (playerInside) { // TODO: implement this
+			// begin drawing on the page
 
-		// TODO: one step further would be to draw the player on top of the page instead
-		// of in it, that way the page never needs to update
-		// Possible solution: have another PGraphics on top just for the player and effects?
-		// this might be perforance heavy in its own way
-		pageGraphics.beginDraw();
+			// TODO: one step further would be to draw the player on top of the page instead
+			// of in it, that way the page never needs to update
+			// Possible solution: have another PGraphics on top just for the player and
+			// effects?
+			// this might be perforance heavy in its own way
+			pageGraphics.beginDraw();
 
-		pageGraphics.translate(-view.getX(), -view.getY());
+			pageGraphics.translate(-view.getX(), -view.getY());
 
-		// draw environment and player
-		pageGraphics.background(240);
+			// draw environment and player
+			pageGraphics.background(240);
 
-		for (Rectangle r : drawFirst) { // draw images
-			if (r instanceof Image) {
-				((Image) r).draw(pageGraphics, scale / size);
+			for (Rectangle r : drawFirst) { // draw images
+				if (r instanceof Image) {
+					((Image) r).draw(pageGraphics, scale / size);
+				}
 			}
+			for (Rectangle r : drawSecond) { // draw tiles and events
+				if (r instanceof Tile) {
+					((Tile) r).draw(pageGraphics, scale / size); // scale is divided by size so that LODs are relative
+																	// to
+																	// page size
+				}
+				if (r instanceof Event && ((Event) r).visible) {
+					((Event) r).draw(pageGraphics, scale / size);
+				}
+			}
+
+			game.player.draw(pageGraphics);
+			// game.paper.draw(pageGraphics, view, scale / size);
+			// end drawing on the page
+			pageGraphics.endDraw();
 		}
-		for (Rectangle r : drawSecond) { // draw tiles and events
-			if (r instanceof Tile) {
-				((Tile) r).draw(pageGraphics, scale / size); // scale is divided by size so that LODs are relative to
-																// page size
-			}
-			if (r instanceof Event && ((Event) r).visible) {
-				((Event) r).draw(pageGraphics, scale / size);
-			}
-		}
-
-		game.player.draw(pageGraphics);
-		//game.paper.draw(pageGraphics, view, scale / size);
-		// end drawing on the page
-		pageGraphics.endDraw();
-		// }
 
 		// draw the page
 		p.imageMode(CENTER);
@@ -139,7 +145,8 @@ public class Page extends Editable {
 		p.rotate(PApplet.radians(angle)); // angle of the page
 		p.scale(flipX, flipY); // flipping the page
 		p.image(pageGraphics, 0, 0, pageGraphics.width, pageGraphics.height); // draw the page
-		p.image(paperGraphics, 0, 0, pageGraphics.width, pageGraphics.height); // draw the page
+		// p.image(paperGraphics, 0, 0, pageGraphics.width, pageGraphics.height); //
+		// draw the page
 		p.popMatrix();
 	}
 
@@ -156,13 +163,31 @@ public class Page extends Editable {
 		// get objects visible to this page
 		pageObjects.clear();
 		game.world.retrieve(pageObjects, view);
-		if(pageRedraw < redrawAmount) {
-			pageRedraw++;
-		}else {
-			pageRedraw = 0;
-		}
+//		if(pageRedraw < redrawAmount) {
+//			pageRedraw++;
+//		}else {
+//			pageRedraw = 0;
+//		}
 		// the step and draw process could be optimized by getting pageObjects once when
 		// the level is run
+		
+		if (view.getTopLeft().x > player.getBottomRight().x - 1) {
+			playerInside = false;
+			return;
+		}
+		if (view.getBottomRight().x < player.getTopLeft().x + 1) {
+			playerInside = false;
+			return;
+		}
+		if (view.getTopLeft().y > player.getBottomRight().y - 1) {
+			playerInside = false;
+			return;
+		}
+		if (view.getBottomRight().y < player.getTopLeft().y + 1) {
+			playerInside = false;
+			return;
+		}
+		playerInside = true;
 	}
 
 	public Set<String> getExcluded() {
