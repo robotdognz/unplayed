adbdd1b041856bf3c95c4ff0baf71cc7cd4a1cc4
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-14 11:09:33 +1300

    adding a player start removes all others

--- Eclipse/Unplayed/src/objects/events/PlayerStart.java
+++ Eclipse/Unplayed/src/objects/events/PlayerStart.java
@@ -1,18 +1,29 @@
 package objects.events;
 
+import java.util.HashSet;
+
 import game.Game;
 import handlers.TextureCache;
 import objects.Event;
+import objects.Rectangle;
 
 public class PlayerStart extends Event {
-	//private Game game;
 
 	public PlayerStart(TextureCache texture, String name, float x, float y, Game game) {
 		super(texture, name, false, x, y, 100, 100);
-		//this.game = game;
-		// TODO set player start position to this position
+		
+		//set player start
 		game.setPlayerStart(x, y);
 		game.createPlayer();
+		
+		//remove all other player starts
+		HashSet<Rectangle> returnSet = new HashSet<Rectangle>();
+		game.world.getAll(returnSet);
+		for(Rectangle r : returnSet) {
+			if(r instanceof PlayerStart) {
+				game.world.remove(r);
+			}
+		}
 	}
 
 }
