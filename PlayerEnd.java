079e9b9c854839939c4be8b612e015eff04ac8a6
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-16 14:01:01 +1300

    fixing player end not working sometimes

--- PlayerEnd.java
+++ PlayerEnd.java
@@ -14,30 +14,30 @@ public class PlayerEnd extends Event {
 		levelEnd = true;
 		newPlayer = new Rectangle(getX(), getY(), getWidth(), getHeight());
 	}
-	
+
 	public Rectangle getNewPlayer() {
 		return newPlayer;
 	}
-	
+
 	@Override
 	public void activate(Game g) {
-		if(getX() != g.player.getX()) {
+		if ((int) getX() != (int) g.player.getX()) {
 			return;
 		}
-		if(getY() != g.player.getY()) {
+		if ((int) getY() != (int) g.player.getY()) {
 			return;
 		}
-		if(getWidth() != g.player.getWidth()) {
+		if ((int) getWidth() != (int) g.player.getWidth()) {
 			return;
 		}
-		if(getHeight() != g.player.getHeight()) {
+		if ((int) getHeight() != (int) g.player.getHeight()) {
 			return;
 		}
-		//if the player is perfectly in the slot
-		if(levelEnd) { //if this is the end of the level
+		// if the player is perfectly in the slot
+		if (levelEnd) { // if this is the end of the level
 			g.endGame();
-		}else { //if this is just part of the puzzle
-			//TODO: spawn a new player
+		} else { // if this is just part of the puzzle
+			// TODO: spawn a new player
 		}
 	}
 
