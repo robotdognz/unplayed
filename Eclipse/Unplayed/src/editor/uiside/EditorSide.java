5edead6b24eacda00dbe585c57a83513761e8bde
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-17 16:05:00 +1300

    more null pointers

--- Eclipse/Unplayed/src/editor/uiside/EditorSide.java
+++ Eclipse/Unplayed/src/editor/uiside/EditorSide.java
@@ -69,7 +69,10 @@ public class EditorSide extends Toolbar {
 	public void draw(PVector touch, Menu menu) {
 		// super.draw(touch, menu);
 
-		if (editor.selected != null || !previousSelected.equals(editor.selected.getClass().getSimpleName())) {
+		if (editor.selected == null) {
+			reset();
+		}
+		if (editor.selected != null && !previousSelected.equals(editor.selected.getClass().getSimpleName())) {
 			reset();
 		}
 
