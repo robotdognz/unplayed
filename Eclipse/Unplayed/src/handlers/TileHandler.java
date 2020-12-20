5ca24068b3f671da2e330931b320fbf13d6de400
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-20 20:47:27 +1300

    testing a more efficent way of drawing images

--- Eclipse/Unplayed/src/handlers/TileHandler.java
+++ Eclipse/Unplayed/src/handlers/TileHandler.java
@@ -9,10 +9,15 @@ public class TileHandler implements Comparable<TileHandler>, Handler {
 	TextureCache texture;
 	File datapath;
 	PImage LOD256;
+	float scale256;
 	PImage LOD128 = null;
+	float scale128;
 	PImage LOD64 = null;
+	float scale64;
 	PImage LOD32 = null;
+	float scale32;
 	PImage LOD16 = null;
+	float scale16;
 
 	public TileHandler(PApplet p, TextureCache texture, File file) {
 		this.p = p;
@@ -23,6 +28,7 @@ public class TileHandler implements Comparable<TileHandler>, Handler {
 		try {
 			LOD256 = p.requestImage(path);
 			LOD256.resize(256, 256);
+			scale256 = 100 / LOD256.width;
 		} catch (Exception e) {
 			// set sprite to file not found image
 		}
@@ -41,29 +47,48 @@ public class TileHandler implements Comparable<TileHandler>, Handler {
 			if (LOD16 == null) {
 				LOD16 = LOD256.get(); // 16
 				LOD16.resize(16, 16);
+				scale16 = 100 / LOD16.width;
 			}
 			return LOD16;
 		} else if (scale > texture.LOD64) {
 			if (LOD32 == null) {
 				LOD32 = LOD256.get(); // 32
 				LOD32.resize(32, 32);
+				scale32 = 100 / LOD32.width;
 			}
 			return LOD32;
 		} else if (scale > texture.LOD128) {
 			if (LOD64 == null) {
 				LOD64 = LOD256.get(); // 64
 				LOD64.resize(64, 64);
+				scale64 = 100 / LOD64.width;
 			}
 			return LOD64;
 		} else if (scale > texture.LOD256) {
 			if (LOD128 == null) {
 				LOD128 = LOD256.get(); // 16
 				LOD128.resize(128, 128);
+				scale128 = 100 / LOD128.width;
 			}
 			return LOD128;
 		} else {
 			return LOD256;
 		}
+	}
+
+	public void drawSprite(PGraphics g, float scale) {
+		if (scale > texture.LOD32) {
+			g.scale(scale16);
+		} else if (scale > texture.LOD64) {
+			g.scale(scale32);
+		} else if (scale > texture.LOD128) {
+			g.scale(scale64);
+		} else if (scale > texture.LOD256) {
+			g.scale(scale128);
+		} else {
+			g.scale(scale256);
+		}
+		g.image(getSprite(scale), 0, 0);
 	}
 
 	@Override
