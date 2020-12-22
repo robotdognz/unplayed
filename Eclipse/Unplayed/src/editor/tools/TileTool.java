ea77d759e7d77153308dee52b2984fdce9c9ee70
robotdognz <61486610+robotdognz@users.noreply.github.com>
2020-12-23 10:49:50 +1300

    erasing tile inside PlayerStart nulls the player

--- Eclipse/Unplayed/src/editor/tools/TileTool.java
+++ Eclipse/Unplayed/src/editor/tools/TileTool.java
@@ -154,6 +154,7 @@ public class TileTool implements Tool {
 				if (required != null && required.equals(editor.selected)) {
 					editor.selected = null;
 				}
+				game.player = null;
 				((PlayerStart) p).setRequired(null);
 			} else if (p instanceof PlayerEnd) {
 				Tile required = ((PlayerEnd) p).getRequired();
