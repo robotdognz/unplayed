b7e5f35b7378eec73c405b929d551d171d6cda5a
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-15 11:39:13 +1300

    horazontal movement seems good, working on vertical

--- Eclipse/Unplayed/src/game/Player.java
+++ Eclipse/Unplayed/src/game/Player.java
@@ -31,7 +31,7 @@ public class Player extends Editable {
 
 	private int jumpCount = 0;
 	private final float playerSpeed = 2.5f; // 10
-	private final int playerGravity = 2;
+	private final float playerGravity = 0.5f; //2
 	private final int terminalVelocity = 50;
 	private final int playerJumpPower = 30;
 	private boolean left = false;
