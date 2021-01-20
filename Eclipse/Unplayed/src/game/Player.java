988db0b0b9c5f20e48062a8014b366964e584c46
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-20 16:43:58 +1300

    adjusting physics player tint

--- Eclipse/Unplayed/src/game/Player.java
+++ Eclipse/Unplayed/src/game/Player.java
@@ -380,9 +380,9 @@ public class Player extends Editable {
 				graphics.translate(pos.x, pos.y);
 				graphics.rotate(-a);
 				if (contactNumber > 0) {
-					graphics.tint(255, 0, 0);
+					graphics.tint(100, 0, 0);
 				} else {
-					graphics.tint(0, 255, 0);
+					graphics.tint(0, 100, 0);
 				}
 
 				graphics.image(tileTexture.getSprite(scale), 0, 0, getWidth(), getHeight());
