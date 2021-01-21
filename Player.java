ffaabf34d0cabef6f89dbf69b6b3abc962a3253f
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-22 10:18:13 +1300

    working on bug

--- Player.java
+++ Player.java
@@ -279,8 +279,9 @@ public class Player extends Editable {
 			if (i > 0) {
 				if (Math.abs(previousX - t.getX()) == 200) {
 					conditionsMet = true;
-					this.dynamicBody.setTransform(box2d.coordPixelsToWorld(pos), angleRounded);
+
 					this.dynamicBody.setFixedRotation(true);
+					this.dynamicBody.setTransform(dynamicBody.getPosition(), angleRounded);
 					return;
 				}
 			}
