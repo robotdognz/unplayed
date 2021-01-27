7e5cc76c59bdf2ff94520696ff7bc46c32362c9d
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-28 12:35:19 +1300

    working on ground slot algorithm

--- Player.java
+++ Player.java
@@ -445,29 +445,33 @@ public class Player extends Editable {
 				// if this tile is the far side of a gap
 				if (Math.abs(previousX - t.getX()) == t.getWidth() + getWidth()) {
 					// make sure the gap is in front of the player
-					if ((direction && t.getBottomRight().x <= pos.x - getWidth() / 2) // moving left
-							|| (!direction && t.getTopLeft().x >= pos.x + getWidth() / 2)) { // moving right
+//					if ((direction && t.getBottomRight().x < pos.x) // moving left
+//							|| (!direction && t.getTopLeft().x > pos.x)) { // moving right
 						// lock rotation
-						this.dynamicBody.setFixedRotation(true);
+//						this.dynamicBody.setFixedRotation(true);
 
 						// create the barrier
 						if (direction) { // moving left
-//							if (t.getBottomRight().x <= pos.x - getWidth() / 2) {
+							if (t.getBottomRight().x <= pos.x - getWidth() / 2) {
+								this.dynamicBody.setFixedRotation(true);
 								Vec2 bottom = new Vec2(t.getBottomRight().x, t.getTopLeft().y);
 								Vec2 top = new Vec2(bottom.x, bottom.y - 5);
 								createBarrier(top, bottom);
-//							}
+								return;
+							}
 
 						} else { // moving right
-//							if (t.getTopLeft().x >= pos.x + getWidth() / 2) {
+							if (t.getTopLeft().x >= pos.x + getWidth() / 2) {
+								this.dynamicBody.setFixedRotation(true);
 								Vec2 bottom = new Vec2(t.getTopLeft().x, t.getTopLeft().y);
 								Vec2 top = new Vec2(bottom.x, bottom.y - 5);
 								createBarrier(top, bottom);
-//							}
+								return;
+							}
 						}
 
-						return;
-					}
+//						return;
+//					}
 				}
 			}
 			previousX = t.getX();
