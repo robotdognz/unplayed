5787ee4f39254cbe2d5c7057113b884e574b2f3f
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-27 16:26:46 +1300

    typo fix

--- PlayerTileXComparator.java
+++ PlayerTileXComparator.java
@@ -8,15 +8,15 @@ public class PlayerTileXComparator implements Comparator<Tile> {
 
 	@Override
 	public int compare(Tile a, Tile b) {
-		float Y = a.getX();
-		float otherY = b.getX();
+		float Y = a.getY();
+		float otherY = b.getY();
 		if (Y > otherY) {
 			return 1;
 		} else if (Y < otherY) {
 			return -1;
 		} else {
-			float X = a.getY();
-			float otherX = b.getY();
+			float X = a.getX();
+			float otherX = b.getX();
 			if (X > otherX) {
 				return 1;
 			} else if (X < otherX) {
