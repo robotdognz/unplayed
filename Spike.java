a2aa6ee0175077b624e91d2907c1b51d360dbbd2
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-29 12:18:44 +1300

    cleaned up spike code, everything should be working now

--- Spike.java
+++ Spike.java
@@ -2,33 +2,28 @@ package objects.events;
 
 import static processing.core.PConstants.CENTER;
 
-//import org.jbox2d.collision.shapes.EdgeShape;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.Fixture;
+//import org.jbox2d.dynamics.Fixture;
 import org.jbox2d.dynamics.FixtureDef;
 
 import game.Game;
-//import game.Player;
 import handlers.TextureCache;
 import objects.Event;
-//import objects.Rectangle;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 
 public class Spike extends Event {
 	private float angle;
-//	private Rectangle bounds;
 	private FixtureDef spikeDef;
-	private Fixture spikeFixture;
+//	private Fixture spikeFixture;
 
 	public Spike(Game game, TextureCache texture, String name, int x, int y) {
 		super(game, texture, name, true, x, y, 100, 100);
 		angle = 0;
-		spikeFixture = null;
-//		createBounds();
+//		spikeFixture = null;
 	}
 
 	@Override
@@ -60,77 +55,12 @@ public class Spike extends Event {
 //			spikeFixtureDef.isSensor = true;
 			spikeDef.userData = "event";
 
-//			createBounds();
-
-			spikeFixture = staticBody.createFixture(spikeDef);
+//			spikeFixure = staticBody.createFixture(spikeDef);
+			staticBody.createFixture(spikeDef);
 
 		}
 
 	}
-
-//	private void createBounds() {
-//		float box2dW = box2d.scalarPixelsToWorld(getWidth() / 2);
-//		float box2dH = box2d.scalarPixelsToWorld(getHeight() / 2);
-//		PolygonShape spikeShape;
-//
-//		if (angle == 0) {
-//			// shape
-//			spikeShape = new PolygonShape();
-//			Vec2 vertices[] = new Vec2[3];
-//			vertices[0] = new Vec2(0, 0);
-//			vertices[1] = new Vec2(-box2dW / 2, -box2dH);
-//			vertices[2] = new Vec2(box2dW / 2, -box2dH);
-//			spikeShape.set(vertices, 3);
-//
-//		} else if (angle == 90) {
-//			// shape
-//			spikeShape = new PolygonShape();
-//			Vec2 vertices[] = new Vec2[3];
-//			vertices[0] = new Vec2(0, 0);
-//			vertices[1] = new Vec2(-box2dW, box2dH / 2);
-//			vertices[2] = new Vec2(-box2dW, -box2dH / 2);
-//			spikeShape.set(vertices, 3);
-//
-//		} else if (angle == 180) {
-//			// shape
-//			spikeShape = new PolygonShape();
-//			Vec2 vertices[] = new Vec2[3];
-//			vertices[0] = new Vec2(0, 0);
-//			vertices[1] = new Vec2(-box2dW / 2, box2dH);
-//			vertices[2] = new Vec2(box2dW / 2, box2dH);
-//			spikeShape.set(vertices, 3);
-//
-//		} else if (angle == 270) {
-//			// shape
-//			spikeShape = new PolygonShape();
-//			Vec2 vertices[] = new Vec2[3];
-//			vertices[0] = new Vec2(0, 0);
-//			vertices[1] = new Vec2(box2dW, box2dH / 2);
-//			vertices[2] = new Vec2(box2dW, -box2dH / 2);
-//			spikeShape.set(vertices, 3);
-//
-//		} else {
-//			return;
-//		}
-//
-//		// fixture
-//		spikeDef = new FixtureDef();
-//		spikeDef.shape = spikeShape;
-////		spikeFixtureDef.isSensor = true;
-//		spikeDef.userData = "event";
-//	}
-
-//	private void createBounds() {
-//		if (angle == 0) {
-//			bounds = new Rectangle(getX() + getWidth() / 3, getY() + getHeight() / 2, getWidth() / 3, getHeight() / 2);
-//		} else if (angle == 90) {
-//			bounds = new Rectangle(getX(), getY() + getHeight() / 3, getWidth() / 2, getHeight() / 3);
-//		} else if (angle == 180) {
-//			bounds = new Rectangle(getX() + getWidth() / 3, getY(), getWidth() / 3, getHeight() / 2);
-//		} else if (angle == 270) {
-//			bounds = new Rectangle(getX() + getWidth() / 2, getY() + getHeight() / 3, getWidth() / 2, getHeight() / 3);
-//		}
-//	}
 
 	public void setAngle(float angle) {
 		this.angle = angle;
@@ -139,11 +69,6 @@ public class Spike extends Event {
 		} else if (this.angle < 0) {
 			this.angle = 360 + this.angle;
 		}
-//		if (spikeFixture != null) {
-//			destroy();
-//			create();
-//		}
-//		createBounds();
 	}
 
 	public void addAngle(float angle) {
@@ -154,7 +79,6 @@ public class Spike extends Event {
 			this.angle = 360 + this.angle;
 		}
 		if (staticBody != null) {
-//			createBounds();
 			destroy();
 			create();
 		}
@@ -167,21 +91,6 @@ public class Spike extends Event {
 
 	@Override
 	public void activate() {
-		// check if player is inside bounds
-//		Player player = game.player;
-//		if (player.getTopLeft().x > bounds.getBottomRight().x - 1) {
-//			return;
-//		}
-//		if (player.getBottomRight().x < bounds.getTopLeft().x + 1) {
-//			return;
-//		}
-//		if (player.getTopLeft().y > bounds.getBottomRight().y - 1) {
-//			return;
-//		}
-//		if (player.getBottomRight().y < bounds.getTopLeft().y + 1) {
-//			return;
-//		}
-
 		game.restart(); // TODO: this needs a custom method in Game
 	}
 
@@ -189,28 +98,28 @@ public class Spike extends Event {
 	public void draw(PGraphics graphics, float scale) {
 		// super.draw(graphics, scale);
 
-		// draw bounds
+		// draw spike
 		graphics.pushMatrix();
-//		graphics.rect(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
 		graphics.imageMode(CENTER);
 		graphics.translate(getX() + getWidth() / 2, getY() + getHeight() / 2);
 		graphics.rotate(PApplet.radians(angle)); // angle of the tile
 		graphics.image(eventTexture.getSprite(scale), 0, 0, getWidth(), getHeight()); // draw the tile
 		graphics.popMatrix();
 
-		if (spikeFixture != null) {
-			graphics.pushMatrix();
+//		//draw hit box
+//		if (spikeFixture != null) {
+//			graphics.pushMatrix();
 //			Vec2 pos = box2d.getBodyPixelCoord(staticBody);
-			graphics.translate(getX() + getWidth() / 2, getY() + getHeight() / 2);
-			graphics.rotate(staticBody.getAngle()); // angle of the spike
-			Vec2 v1 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(0));
-			Vec2 v2 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(1));
-			Vec2 v3 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(2));
-			graphics.stroke(0, 0, 255);
-			graphics.strokeWeight(4);
-			graphics.triangle(v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
-			graphics.popMatrix();
-		}
+//			graphics.translate(pos.x, pos.y);
+//			graphics.rotate(staticBody.getAngle()); // angle of the spike
+//			Vec2 v1 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(0));
+//			Vec2 v2 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(1));
+//			Vec2 v3 = box2d.coordWorldToPixels(((PolygonShape) spikeFixture.getShape()).getVertex(2));
+//			graphics.stroke(0, 0, 255);
+//			graphics.strokeWeight(4);
+//			graphics.triangle(v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
+//			graphics.popMatrix();
+//		}
 	}
 
 }
