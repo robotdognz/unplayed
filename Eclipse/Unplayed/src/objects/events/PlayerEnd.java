dcddabcef79f7daa7ae44d8d3882361c9cacdb22
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-01-29 13:53:26 +1300

    found the issue

--- Eclipse/Unplayed/src/objects/events/PlayerEnd.java
+++ Eclipse/Unplayed/src/objects/events/PlayerEnd.java
@@ -150,20 +150,20 @@ public class PlayerEnd extends Event {
 			playerAngle = Math.round(playerAngle / 90) * 90; //round to nearest 90
 			
 			//get it into the 360 range
-			while (Math.abs(playerAngle) > 360) {
+			while (Math.abs(playerAngle) > 270) {
 				if (playerAngle > 0) {
-					playerAngle -= 90;
+					playerAngle -= 360;
 				} else {
-					playerAngle += 90;
+					playerAngle += 360;
 				}
 			}
 			if (playerAngle < 0) {
 				playerAngle += 360;
 			}
 //
-			if (playerAngle == 360) {
-				playerAngle = 0;
-			}
+//			if (playerAngle == 360) {
+//				playerAngle = 0;
+//			}
 
 			if (!(player.getAngle() == required.getAngle())) {
 				return;
