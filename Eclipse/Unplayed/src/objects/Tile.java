336b9a84dffc1692fe9385ea9f87efc556cb9339
robotdognz <61486610+robotdognz@users.noreply.github.com>
2021-02-10 11:16:01 +1300

    so janky

--- Eclipse/Unplayed/src/objects/Tile.java
+++ Eclipse/Unplayed/src/objects/Tile.java
@@ -99,14 +99,14 @@ public class Tile extends Editable implements Comparable<Tile> {
 			leftEdgeDef.shape = leftEdge;
 			leftEdgeDef.density = density;
 			leftEdgeDef.friction = friction;
-//			leftEdgeDef.userData = "wall";
+			leftEdgeDef.userData = "wall";
 			staticBody.createFixture(leftEdgeDef);
 			// right edge
 			FixtureDef rightEdgeDef = new FixtureDef();
 			rightEdgeDef.shape = rightEdge;
 			rightEdgeDef.density = density;
 			rightEdgeDef.friction = friction;
-//			rightEdgeDef.userData = "wall";
+			rightEdgeDef.userData = "wall";
 			staticBody.createFixture(rightEdgeDef);
 
 //			// left wall sensor
