8858d3b47fada5e5fa983f031186b577544b3e6f
robotdognz <robotdognz@gmail.com>
2021-08-25 11:02:18 +1200

    Updated file copying batch file to work on new laptop

--- A_Create.bat
+++ A_Create.bat
@@ -1,3 +1,3 @@
-md "C:\Users\robot\Documents\GitHub\unplayed"
-cd /d "C:\Users\robot\Documents\GitHub\unplayed\Eclipse\Unplayed\src"
-for /r %%d in (*) do copy "%%d" "C:\Users\robot\Documents\GitHub\unplayed"
+md "C:\Users\robot\Documents\Unplayed\unplayed"
+cd /d "C:\Users\robot\Documents\Unplayed\unplayed\Eclipse\Unplayed\src"
+for /r %%d in (*) do copy "%%d" "C:\Users\robot\Documents\Unplayed\unplayed"
