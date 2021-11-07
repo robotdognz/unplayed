d28ae788c37b64a1791b535b0798c09824294468
robotdognz <robotdognz@gmail.com>
2021-11-08 11:11:22 +1300

    Cleaning up texture handler classes, better code reuse, started work on cleaning up width and hight to use grid count instead of old arbitarariy multiplications of 100

--- Eclipse/Unplayed/src/handlers/Handler.java
+++ Eclipse/Unplayed/src/handlers/Handler.java
@@ -2,16 +2,92 @@ package handlers;
 
 import java.io.File;
 
+import processing.core.PApplet;
 import processing.core.PImage;
 
-public interface Handler {
-	public PImage getSprite(float scale);
+public abstract class Handler {
+	PApplet p;
+	TextureCache texture;
+	File datapath;
+	
+	PImage LOD256;
+	PImage LOD128 = null;
+	PImage LOD64 = null;
+	PImage LOD32 = null;
+	PImage LOD16 = null;
+	
+	private int pWidth;
+	private int pHeight;
+	
+	public Handler(PApplet p, TextureCache texture, File file, int pWidth, int pHeight) {
+		this.p = p;
+		this.texture = texture;
+		this.datapath = file;
+		
+		this.pWidth = pWidth;
+		this.pHeight = pHeight;
+		
+		String path = file.toString();
+		
+		try {
+			LOD256 = p.requestImage(path);
+			LOD256.resize(256 * pWidth, 256 * pHeight);
+		} catch (Exception e) {
+			// set sprite to file not found image
+		}
+	}
+	
+	public PImage getSprite(float scale) {
+		if (scale > texture.LOD32) {
+			if (LOD16 == null) {
+				LOD16 = LOD256.get(); // 16
+				LOD16.resize(16 * pWidth, 16 * pHeight);
+			}
+			return LOD16;
+		} else if (scale > texture.LOD64) {
+			if (LOD32 == null) {
+				LOD32 = LOD256.get(); // 32
+				LOD32.resize(32 * pWidth, 32 * pHeight);
+			}
+			return LOD32;
+		} else if (scale > texture.LOD128) {
+			if (LOD64 == null) {
+				LOD64 = LOD256.get(); // 64
+				LOD64.resize(64 * pWidth, 64 * pHeight);
+			}
+			return LOD64;
+		} else if (scale > texture.LOD256) {
+			if (LOD128 == null) {
+				LOD128 = LOD256.get(); // 16
+				LOD128.resize(128 * pWidth, 128 * pHeight);
+			}
+			return LOD128;
+		} else {
+			return LOD256;
+		}
+	}
 
-	public File getFile();
+	public File getFile() {
+		return datapath;
+	}
 
-	public int getWidth();
+	public int getWidth() {
+		return pWidth*100; //TODO: find an correct places where the *100 is needed and clean this up
+	}
 
-	public int getHeight();
+	public int getHeight() {
+		return pHeight*100;
+	}
 
-	public void draw(float pX, float pY, float size);
+	public void draw(float pX, float pY, float size) {
+		// calculate how to scale the image so it appears in the scroll bar correctly
+		float scaleFactor;
+		if (getWidth() >= getHeight()) {
+			scaleFactor = size / getWidth();
+		} else {
+			scaleFactor = size / getHeight();
+		}
+		// draw the scaled image
+		p.image(getSprite(6), pX, pY, pWidth * scaleFactor, pHeight * scaleFactor);
+	}
 }
--- ImageHandler.java
+++ ImageHandler.java
@@ -4,97 +4,100 @@ import java.io.File;
 
 import processing.core.*;
 
-public class ImageHandler implements Comparable<ImageHandler>, Handler {
-	PApplet p;
-	TextureCache texture;
-	File datapath;
-	PImage LOD256;
-	PImage LOD128 = null;
-	PImage LOD64 = null;
-	PImage LOD32 = null;
-	PImage LOD16 = null;
-	int pWidth;
-	int pHeight;
+public class ImageHandler extends Handler implements Comparable<ImageHandler> {
+//	PApplet p;
+//	TextureCache texture;
+//	File datapath;
+//	PImage LOD256;
+//	PImage LOD128 = null;
+//	PImage LOD64 = null;
+//	PImage LOD32 = null;
+//	PImage LOD16 = null;
+//	int pWidth;
+//	int pHeight;
 
 	public ImageHandler(PApplet p, TextureCache texture, File file, int pWidth, int pHeight) {
-		this.p = p;
-		this.texture = texture;
-		datapath = file;
-		this.pWidth = pWidth * 100; // these are turned from grid amount to draw units for the level
-		this.pHeight = pHeight * 100;
-		String path = file.toString();
-
-		try {
-			LOD256 = p.requestImage(path);
-			LOD256.resize(256 * pWidth, 256 * pHeight);
-//			LOD256.resize((int) (p.width / 5.625) * pWidth, (int) (p.width / 5.625) * pHeight);
-		} catch (Exception e) {
-			// set sprite to file not found image
-		}
+		super(p, texture, file, pWidth, pHeight);
+		
+//		this.p = p;
+//		this.texture = texture;
+//		datapath = file;
+		
+//		this.pWidth = pWidth * 100; // these are turned from grid amount to draw units for the level
+//		this.pHeight = pHeight * 100;
+//		String path = file.toString();
+//
+//		try {
+//			LOD256 = p.requestImage(path);
+//			LOD256.resize(256 * pWidth, 256 * pHeight);
+////			LOD256.resize((int) (p.width / 5.625) * pWidth, (int) (p.width / 5.625) * pHeight);
+//		} catch (Exception e) {
+//			// set sprite to file not found image
+//		}
 	}
 
-	@Override
-	public int getWidth() {
-		return pWidth;
-	}
+//	@Override
+//	public int getWidth() {
+//		return pWidth*100;
+//	}
+//
+//	@Override
+//	public int getHeight() {
+//		return pHeight*100;
+//	}
 
-	@Override
-	public int getHeight() {
-		return pHeight;
-	}
+//	@Override
+//	public PImage getSprite(float scale) {
+//		if (scale > texture.LOD32) {
+//			if (LOD16 == null) {
+//				LOD16 = LOD256.get(); // 16
+//				LOD16.resize(16 * pWidth / 100, 16 * pHeight / 100);
+////				LOD16.resize((int) (p.width / 90) * (pWidth/100), (int) (p.width / 90) * (pHeight/100));
+//			}
+//			return LOD16;
+//		} else if (scale > texture.LOD64) {
+//			if (LOD32 == null) {
+//				LOD32 = LOD256.get(); // 32
+//				LOD32.resize(32 * pWidth / 100, 32 * pHeight / 100);
+////				LOD32.resize((int) (p.width / 45) * (pWidth/100), (int) (p.width / 45) * (pHeight/100));
+//			}
+//			return LOD32;
+//		} else if (scale > texture.LOD128) {
+//			if (LOD64 == null) {
+//				LOD64 = LOD256.get(); // 64
+//				LOD64.resize(64 * pWidth / 100, 64 * pHeight / 100);
+////				LOD64.resize((int) (p.width / 22.5) * (pWidth/100), (int) (p.width / 22.5) * (pHeight/100));
+//			}
+//			return LOD64;
+//		} else if (scale > texture.LOD256) {
+//			if (LOD128 == null) {
+//				LOD128 = LOD256.get(); // 16
+//				LOD128.resize(128 * pWidth / 100, 128 * pHeight / 100);
+////				LOD128.resize((int) (p.width / 11.25) * (pWidth/100), (int) (p.width / 11.25) * (pHeight/100));
+//			}
+//			return LOD128;
+//		} else {
+//			return LOD256;
+//		}
+//	}
 
-	@Override
-	public PImage getSprite(float scale) {
-		if (scale > texture.LOD32) {
-			if (LOD16 == null) {
-				LOD16 = LOD256.get(); // 16
-				LOD16.resize(16 * pWidth / 100, 16 * pHeight / 100);
-//				LOD16.resize((int) (p.width / 90) * (pWidth/100), (int) (p.width / 90) * (pHeight/100));
-			}
-			return LOD16;
-		} else if (scale > texture.LOD64) {
-			if (LOD32 == null) {
-				LOD32 = LOD256.get(); // 32
-				LOD32.resize(32 * pWidth / 100, 32 * pHeight / 100);
-//				LOD32.resize((int) (p.width / 45) * (pWidth/100), (int) (p.width / 45) * (pHeight/100));
-			}
-			return LOD32;
-		} else if (scale > texture.LOD128) {
-			if (LOD64 == null) {
-				LOD64 = LOD256.get(); // 64
-				LOD64.resize(64 * pWidth / 100, 64 * pHeight / 100);
-//				LOD64.resize((int) (p.width / 22.5) * (pWidth/100), (int) (p.width / 22.5) * (pHeight/100));
-			}
-			return LOD64;
-		} else if (scale > texture.LOD256) {
-			if (LOD128 == null) {
-				LOD128 = LOD256.get(); // 16
-				LOD128.resize(128 * pWidth / 100, 128 * pHeight / 100);
-//				LOD128.resize((int) (p.width / 11.25) * (pWidth/100), (int) (p.width / 11.25) * (pHeight/100));
-			}
-			return LOD128;
-		} else {
-			return LOD256;
-		}
-	}
+//	@Override
+//	public File getFile() {
+//		return datapath;
+//	}
 
-	@Override
-	public File getFile() {
-		return datapath;
-	}
-
-	@Override
-	public void draw(float pX, float pY, float size) {
-		// calculate how to scale the image so it appears in the scroll bar correctly
-		float scaleFactor;
-		if (getWidth() >= getHeight()) {
-			scaleFactor = size / getWidth();
-		} else {
-			scaleFactor = size / getHeight();
-		}
-		// draw the scaled image
-		p.image(getSprite(6), pX, pY, pWidth * scaleFactor, pHeight * scaleFactor);
-	}
+//	@Override
+//	public void draw(float pX, float pY, float size) {
+//		// calculate how to scale the image so it appears in the scroll bar correctly
+//		float scaleFactor;
+//		if (getWidth() >= getHeight()) {
+//			scaleFactor = size / getWidth();
+//		} else {
+//			scaleFactor = size / getHeight();
+//		}
+//		// draw the scaled image
+//		p.image(getSprite(6), pX, pY, pWidth * scaleFactor, pHeight * scaleFactor);
+//	}
 
 	@Override
 	public int compareTo(ImageHandler otherImageHandler) {
