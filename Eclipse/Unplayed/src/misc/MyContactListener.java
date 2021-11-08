16e78c380f7aca53c0f73e04bb0e51d0e525addc
robotdognz <robotdognz@gmail.com>
2021-11-08 14:46:37 +1300

    Changed if else blocks to switch cases for contact listener, made code more maintainable removing duplicates and replacing with a single method

--- Eclipse/Unplayed/src/misc/MyContactListener.java
+++ Eclipse/Unplayed/src/misc/MyContactListener.java
@@ -14,93 +14,181 @@ import objects.Tile;
 public class MyContactListener implements ContactListener {
 	Game game;
 
+	// variables used in begin and end contact
+	private Player player = null;
+	private Tile tile = null;
+	private Event event = null;
+	private boolean playerBody = false; // one of the fixtures is the player
+	private boolean playerSensor = false; // one of the fixtures is the player's sensor
+	private boolean ground = false; // one of the fixtures is the ground
+	private boolean wall = false; // one of the fixtures is a wall
+	private boolean roofBarrier = false; // one of the fixtures is a roof barrier
+
 	public MyContactListener(Game game) {
 		this.game = game;
 	}
-	
+
 	// TODO: change these if else chains to switch cases
+
+	private void updateVariables(Fixture fixture) {
+		Object fixtureUserData = fixture.getUserData();
+		if (fixtureUserData != null) {
+			if (fixtureUserData instanceof CollisionEnum) {
+				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+				switch (userData) {
+				case PLAYER_BODY:
+					player = (Player) fixture.getBody().getUserData();
+					playerBody = true;
+					break;
+				case TILE:
+					tile = (Tile) fixture.getBody().getUserData();
+					break;
+				case PLAYER_SENSOR:
+					player = (Player) fixture.getBody().getUserData();
+					playerSensor = true;
+					break;
+				case GROUND:
+					ground = true;
+					break;
+				case WALL:
+					wall = true;
+					break;
+				case EVENT:
+					event = (Event) fixture.getBody().getUserData();
+					break;
+				case ROOF_BARRIER:
+					roofBarrier = true;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
 
 	@Override
 	public void beginContact(Contact contact) {
-		Fixture fixtureA = contact.getFixtureA();
-		Fixture fixtureB = contact.getFixtureB();
+//		Fixture fixtureA = contact.getFixtureA();
+//		Fixture fixtureB = contact.getFixtureB();
 
-		Player player = null;
-		Tile tile = null;
-		Event event = null;
-		boolean playerBody = false; // one of the fixtures is the player
-		boolean playerSensor = false; // one of the fixtures is the player's sensor
-		boolean ground = false; // one of the fixtures is the ground
-		boolean wall = false; // one of the fixtures is a wall
-		boolean roofBarrier = false; // one of the fixtures is a roof barrier
+//		Fixture[] fixtures = { contact.getFixtureA(), contact.getFixtureB() };
 
-		// check fixture
-		Object fixtureUserData = fixtureA.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+		// reset variables
+		this.player = null;
+		this.tile = null;
+		this.event = null;
+		this.playerBody = false;
+		this.playerSensor = false;
+		this.ground = false;
+		this.wall = false;
+		this.roofBarrier = false;
 
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureA.getBody().getUserData();
-					playerBody = true;
+		updateVariables(contact.getFixtureA());
+		updateVariables(contact.getFixtureB());
 
-				} else if (userData == CollisionEnum.TILE) {
-					tile = (Tile) fixtureA.getBody().getUserData();
+//		for (Fixture fixture : fixtures) {
+//			Object fixtureUserData = fixture.getUserData();
+//			if (fixtureUserData != null) {
+//				if (fixtureUserData instanceof CollisionEnum) {
+//					CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//					switch (userData) {
+//					case PLAYER_BODY:
+//						player = (Player) fixture.getBody().getUserData();
+//						playerBody = true;
+//						break;
+//					case TILE:
+//						tile = (Tile) fixture.getBody().getUserData();
+//						break;
+//					case PLAYER_SENSOR:
+//						player = (Player) fixture.getBody().getUserData();
+//						playerSensor = true;
+//						break;
+//					case GROUND:
+//						ground = true;
+//						break;
+//					case WALL:
+//						wall = true;
+//						break;
+//					case EVENT:
+//						event = (Event) fixture.getBody().getUserData();
+//						break;
+//					case ROOF_BARRIER:
+//						roofBarrier = true;
+//						break;
+//					default:
+//						break;
+//					}
+//				}
+//			}
+//		}
 
-				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
-					player = (Player) fixtureA.getBody().getUserData();
-					playerSensor = true;
-
-				} else if (userData == CollisionEnum.GROUND) { 
-					ground = true;
-
-				} else if (userData == CollisionEnum.WALL) {
-					wall = true;
-
-				} else if (userData == CollisionEnum.EVENT) { 
-					event = (Event) fixtureA.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.ROOF_BARRIER) { 
-					roofBarrier = true;
-
-				}
-
-			}
-		}
+//		// check fixture
+//		Object fixtureUserData = fixtureA.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureA.getBody().getUserData();
+//					playerBody = true;
+//
+//				} else if (userData == CollisionEnum.TILE) {
+//					tile = (Tile) fixtureA.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
+//					player = (Player) fixtureA.getBody().getUserData();
+//					playerSensor = true;
+//
+//				} else if (userData == CollisionEnum.GROUND) {
+//					ground = true;
+//
+//				} else if (userData == CollisionEnum.WALL) {
+//					wall = true;
+//
+//				} else if (userData == CollisionEnum.EVENT) {
+//					event = (Event) fixtureA.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.ROOF_BARRIER) {
+//					roofBarrier = true;
+//
+//				}
+//
+//			}
+//		}
 
 		// check fixture B
-		fixtureUserData = fixtureB.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
-
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureB.getBody().getUserData();
-					playerBody = true;
-
-				} else if (userData == CollisionEnum.TILE) {
-					tile = (Tile) fixtureB.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
-					player = (Player) fixtureB.getBody().getUserData();
-					playerSensor = true;
-
-				} else if (userData == CollisionEnum.GROUND) {
-					ground = true;
-
-				} else if (userData == CollisionEnum.WALL) {
-					wall = true;
-
-				} else if (userData == CollisionEnum.EVENT) {
-					event = (Event) fixtureB.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.ROOF_BARRIER) {
-					roofBarrier = true;
-
-				}
-
-			}
-		}
+//		fixtureUserData = fixtureB.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureB.getBody().getUserData();
+//					playerBody = true;
+//
+//				} else if (userData == CollisionEnum.TILE) {
+//					tile = (Tile) fixtureB.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
+//					player = (Player) fixtureB.getBody().getUserData();
+//					playerSensor = true;
+//
+//				} else if (userData == CollisionEnum.GROUND) {
+//					ground = true;
+//
+//				} else if (userData == CollisionEnum.WALL) {
+//					wall = true;
+//
+//				} else if (userData == CollisionEnum.EVENT) {
+//					event = (Event) fixtureB.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.ROOF_BARRIER) {
+//					roofBarrier = true;
+//
+//				}
+//
+//			}
+//		}
 
 		// if on of them is the player and one is the ground
 		if (ground && playerBody) {
@@ -130,83 +218,134 @@ public class MyContactListener implements ContactListener {
 
 	@Override
 	public void endContact(Contact contact) {
-		Fixture fixtureA = contact.getFixtureA();
-		Fixture fixtureB = contact.getFixtureB();
+//		Fixture fixtureA = contact.getFixtureA();
+//		Fixture fixtureB = contact.getFixtureB();
 
-		Player player = null;
-		Tile tile = null;
-		Event event = null;
-		boolean playerBody = false; // one of the fixtures is the player
-		boolean playerSensor = false; // one of the fixtures is the player's sensor
-		boolean ground = false; // one of the fixtures is the ground
-		boolean wall = false; // one of the fixtures is a wall
-		boolean roofBarrier = false; // one of the fixtures is a roof barrier
+		// reset variables
+		this.player = null;
+		this.tile = null;
+		this.event = null;
+		this.playerBody = false;
+		this.playerSensor = false;
+		this.ground = false;
+		this.wall = false;
+		this.roofBarrier = false;
+
+		updateVariables(contact.getFixtureA());
+		updateVariables(contact.getFixtureB());
+
+//		Fixture[] fixtures = { contact.getFixtureA(), contact.getFixtureB() };
+//
+//		Player player = null;
+//		Tile tile = null;
+//		Event event = null;
+//		boolean playerBody = false; // one of the fixtures is the player
+//		boolean playerSensor = false; // one of the fixtures is the player's sensor
+//		boolean ground = false; // one of the fixtures is the ground
+//		boolean wall = false; // one of the fixtures is a wall
+//		boolean roofBarrier = false; // one of the fixtures is a roof barrier
+//
+//		for (Fixture fixture : fixtures) {
+//			Object fixtureUserData = fixture.getUserData();
+//			if (fixtureUserData != null) {
+//				if (fixtureUserData instanceof CollisionEnum) {
+//					CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//					switch (userData) {
+//					case PLAYER_BODY:
+//						player = (Player) fixture.getBody().getUserData();
+//						playerBody = true;
+//						break;
+//					case TILE:
+//						tile = (Tile) fixture.getBody().getUserData();
+//						break;
+//					case PLAYER_SENSOR:
+//						player = (Player) fixture.getBody().getUserData();
+//						playerSensor = true;
+//						break;
+//					case GROUND:
+//						ground = true;
+//						break;
+//					case WALL:
+//						wall = true;
+//						break;
+//					case EVENT:
+//						event = (Event) fixture.getBody().getUserData();
+//						break;
+//					case ROOF_BARRIER:
+//						roofBarrier = true;
+//						break;
+//					default:
+//						break;
+//					}
+//				}
+//			}
+//		}
 
 		// check fixture A
-		Object fixtureUserData = fixtureA.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
-
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureA.getBody().getUserData();
-					playerBody = true;
-
-				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
-					player = (Player) fixtureA.getBody().getUserData();
-					playerSensor = true;
-
-				} else if (userData == CollisionEnum.EVENT) {
-					event = (Event) fixtureA.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.TILE) {
-					tile = (Tile) fixtureA.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.GROUND) {
-					ground = true;
-
-				} else if (userData == CollisionEnum.WALL) {
-					wall = true;
-
-				} else if (userData == CollisionEnum.ROOF_BARRIER) {
-					roofBarrier = true;
-
-				}
-			}
-		}
+//		Object fixtureUserData = fixtureA.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureA.getBody().getUserData();
+//					playerBody = true;
+//
+//				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
+//					player = (Player) fixtureA.getBody().getUserData();
+//					playerSensor = true;
+//
+//				} else if (userData == CollisionEnum.EVENT) {
+//					event = (Event) fixtureA.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.TILE) {
+//					tile = (Tile) fixtureA.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.GROUND) {
+//					ground = true;
+//
+//				} else if (userData == CollisionEnum.WALL) {
+//					wall = true;
+//
+//				} else if (userData == CollisionEnum.ROOF_BARRIER) {
+//					roofBarrier = true;
+//
+//				}
+//			}
+//		}
 
 		// check fixture B
-		fixtureUserData = fixtureB.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
-
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureB.getBody().getUserData();
-					playerBody = true;
-
-				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
-					player = (Player) fixtureB.getBody().getUserData();
-					playerSensor = true;
-
-				} else if (userData == CollisionEnum.EVENT) {
-					event = (Event) fixtureB.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.TILE) {
-					tile = (Tile) fixtureB.getBody().getUserData();
-
-				} else if (userData == CollisionEnum.GROUND) {
-					ground = true;
-
-				} else if (userData == CollisionEnum.WALL) {
-					wall = true;
-
-				} else if (userData == CollisionEnum.ROOF_BARRIER) {
-					roofBarrier = true;
-
-				}
-			}
-		}
+//		fixtureUserData = fixtureB.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//				
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureB.getBody().getUserData();
+//					playerBody = true;
+//
+//				} else if (userData == CollisionEnum.PLAYER_SENSOR) {
+//					player = (Player) fixtureB.getBody().getUserData();
+//					playerSensor = true;
+//
+//				} else if (userData == CollisionEnum.EVENT) {
+//					event = (Event) fixtureB.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.TILE) {
+//					tile = (Tile) fixtureB.getBody().getUserData();
+//
+//				} else if (userData == CollisionEnum.GROUND) {
+//					ground = true;
+//
+//				} else if (userData == CollisionEnum.WALL) {
+//					wall = true;
+//
+//				} else if (userData == CollisionEnum.ROOF_BARRIER) {
+//					roofBarrier = true;
+//
+//				}
+//			}
+//		}
 
 		// if one of them is the player sensor and one is a tile
 		if (playerSensor && tile != null) {
@@ -236,34 +375,49 @@ public class MyContactListener implements ContactListener {
 
 	@Override
 	public void postSolve(Contact contact, ContactImpulse impulse) {
-		Fixture fixtureA = contact.getFixtureA();
-		Fixture fixtureB = contact.getFixtureB();
+//		Fixture fixtureA = contact.getFixtureA();
+//		Fixture fixtureB = contact.getFixtureB();
+
+		Fixture[] fixtures = { contact.getFixtureA(), contact.getFixtureB() };
 
 		Player player = null;
 
-		// check fixture A
-		Object fixtureUserData = fixtureA.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+		for (Fixture fixture : fixtures) {
+			Object fixtureUserData = fixture.getUserData();
+			if (fixtureUserData != null) {
+				if (fixtureUserData instanceof CollisionEnum) {
+					CollisionEnum userData = (CollisionEnum) fixtureUserData;
 
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureA.getBody().getUserData();
+					if (userData == CollisionEnum.PLAYER_BODY) {
+						player = (Player) fixture.getBody().getUserData();
+					}
 				}
 			}
 		}
 
-		// check fixture B
-		fixtureUserData = fixtureB.getUserData();
-		if (fixtureUserData != null) {
-			if (fixtureUserData instanceof CollisionEnum) {
-				CollisionEnum userData = (CollisionEnum) fixtureUserData;
-
-				if (userData == CollisionEnum.PLAYER_BODY) {
-					player = (Player) fixtureB.getBody().getUserData();
-				}
-			}
-		}
+//		// check fixture A
+//		Object fixtureUserData = fixtureA.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureA.getBody().getUserData();
+//				}
+//			}
+//		}
+//
+//		// check fixture B
+//		fixtureUserData = fixtureB.getUserData();
+//		if (fixtureUserData != null) {
+//			if (fixtureUserData instanceof CollisionEnum) {
+//				CollisionEnum userData = (CollisionEnum) fixtureUserData;
+//
+//				if (userData == CollisionEnum.PLAYER_BODY) {
+//					player = (Player) fixtureB.getBody().getUserData();
+//				}
+//			}
+//		}
 
 		if (player != null) {
 			player.physicsImpact(impulse.normalImpulses);
