73f56d406a64f56aa384de21b27ac6916756eb9b
robotdognz <robotdognz@gmail.com>
2021-11-11 12:37:51 +1300

    Rotation smoothing should now proprly use delta time and adjust for different rotation amounts

--- RotationSmooth.java
+++ RotationSmooth.java
@@ -6,8 +6,6 @@ public class RotationSmooth {
 
 	private boolean isFinished = false;
 
-	private int steps;
-	private int step;
 	private float stepSize;
 
 	public RotationSmooth(float oldAngle, float newAngle) {
@@ -16,38 +14,28 @@ public class RotationSmooth {
 
 		float difference = Math.abs(oldAngle - newAngle);
 
-//		this.step = 0;
-//		this.steps = (int) (difference / 5.0);
-//
+		float animationLength = difference / 220;// 0.1f; // portion of a second it takes the animation to finish
+
 		if (newAngle < oldAngle) {
 			difference = -difference;
 		}
-//
-//		this.stepSize = difference * (1 / (float) steps);
-
-		float animationLength = 0.5f; // half a second
 
 		this.stepSize = difference / animationLength;
 	}
 
 	public void deltaStep(float deltaTime) {
-		
-		// if the diff between the stepped angle and new angle is less than the diff between
+
+		// if the diff between the stepped angle and new angle is less than the diff
+		// between
 		// old angle and new angle, step
 		// else do nothing and set is finished
-		
-//		if(Math.abs(deltaTime))
-		
-		oldAngle += stepSize*deltaTime;
 
-		if (Math.abs(oldAngle - newAngle) <= 3) { // arbitrary number
+		oldAngle += stepSize * deltaTime;
+
+		if (Math.abs(oldAngle - newAngle) <= 3) { // 3 is an arbitrary number
 			isFinished = true;
 		}
 
-//		step += 1;
-//		if (step == steps) {
-//			isFinished = true;
-//		}
 	}
 
 	public float getAngle() {
