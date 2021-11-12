22aa72ed821e307d2c00cd0c45a56e9c28bbb7df
robotdognz <robotdognz@gmail.com>
2021-11-12 16:00:08 +1300

    Might be on to something

--- Player.java
+++ Player.java
@@ -393,8 +393,9 @@ public class Player extends Editable {
 			// create a rotationSmooth to smooth over the angle adjustment
 			// only if there is a reasonable difference
 //			if (Math.abs(oldAngle - newAngle) > 2) {
-			rotationSmooth = new RotationSmooth(oldAngle, newAngle, vel, angularVel);
 			DebugOutput.pushMessage("" + PApplet.nf(vibration.getImpactHistory() / 1000, 0, 1), 5);
+			rotationSmooth = new RotationSmooth(oldAngle, newAngle, vel, angularVel);
+			
 //			DebugOutput.pushMessage("" + vibration.impacts.get(vibration.impacts.size()-1), 5);
 //			}
 
