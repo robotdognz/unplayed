3f2f8798c95b4d54e8ff147112dde54fa8d5ad60
robotdognz <robotdognz@gmail.com>
2022-01-06 11:49:53 +1300

    More adjustment

--- Eclipse/Unplayed/src/game/BackgroundPaper.java
+++ Eclipse/Unplayed/src/game/BackgroundPaper.java
@@ -11,7 +11,7 @@ public class BackgroundPaper {
 
 	public BackgroundPaper(TextureCache texture) {
 		this.texture = texture;
-		gridSize = 1600; // 400
+		gridSize = 1000; // 400
 	}
 
 	public void draw(PGraphics graphics, Rectangle screen, float scale) {
