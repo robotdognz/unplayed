ce5452bd043b018063b2d5acf71407d7c8004e46
robotdognz <robotdognz@gmail.com>
2022-01-11 13:44:51 +1300

    Again with this

--- Page.java
+++ Page.java
@@ -202,7 +202,7 @@ public class Page extends Editable {
 		p.fill(240);
 		p.rect(0, 0, view.getWidth(), view.getHeight());
 
-		p.translate(-view.getX() - view.getWidth() * 0.5, -view.getY());
+		p.translate((float) -(view.getX() - view.getWidth() * 0.5), (float) -(view.getY() - view.getHeight() * 0.5));
 
 		// draw tiles and images
 		for (Rectangle r : pageObjects) { // draw images
