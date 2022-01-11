a9eb201e7b392cd492192d1cad928bceb5bbe0d4
robotdognz <robotdognz@gmail.com>
2022-01-11 13:34:37 +1300

    More testing

--- Page.java
+++ Page.java
@@ -201,6 +201,8 @@ public class Page extends Editable {
 		// draw the page background
 		p.fill(240);
 		p.rect(0, 0, view.getWidth(), view.getHeight());
+		
+		p.translate(view.getX(), view.getY());
 
 		// draw tiles and images
 		for (Rectangle r : pageObjects) { // draw images
