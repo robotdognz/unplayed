f4637846b42c68b5ddd6fa5d8d26dca74dc2c14d
robotdognz <robotdognz@gmail.com>
2022-01-12 12:03:35 +1300

    Page view background is causing issues

--- PageView.java
+++ PageView.java
@@ -72,8 +72,8 @@ public class PageView {
 		}
 
 		// draw the looping background
-//		p.background(100);
-		paper.draw(p.getGraphics(), topLeft, bottomRight, currentScale*2); // background paper effect
+		p.background(100);
+//		paper.draw(p.getGraphics(), topLeft, bottomRight, currentScale*2); // background paper effect
 
 		// draw backgrounds that are inside that area
 		for (Background background : backgrounds) {
