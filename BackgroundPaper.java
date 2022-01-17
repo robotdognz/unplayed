ea56302e2ad4e5b68c2cc1e5de792a7ae1c28156
robotdognz <robotdognz@gmail.com>
2022-01-18 11:05:23 +1300

    Background paper algorithm should now be much more graphics rendering efficent

--- BackgroundPaper.java
+++ BackgroundPaper.java
@@ -11,21 +11,6 @@ public class BackgroundPaper {
 	private TextureCache texture;
 	int gridSize;
 
-	// rendering algorithm variables
-	float startX;
-	float startY;
-	float endX;
-	float endY;
-//	PImage image;
-//	float leftEdge;
-//	float topEdge;
-//	float rightEdge;
-//	float bottomEdge;
-//	int imageStartX;
-//	int imageStartY;
-//	int imageEndX;
-//	int imageEndY;
-
 	public BackgroundPaper(PApplet p, TextureCache texture) {
 		this.texture = texture;
 		gridSize = 1000; // 1000
@@ -34,37 +19,19 @@ public class BackgroundPaper {
 
 	public void draw(PGraphics graphics, PVector topLeft, PVector bottomRight, float scale) {
 
-//		// find x start position
-//		startX = (int) Math.round((topLeft.x - (gridSize / 2)) / gridSize) * gridSize;
-//		// find y start position
-//		startY = (int) Math.round((topLeft.y - (gridSize / 2)) / gridSize) * gridSize;
-//		// find x end position
-//		endX = (int) Math.round((bottomRight.x + (gridSize / 2)) / gridSize) * gridSize;
-//		// find y end position
-//		endY = (int) Math.round((bottomRight.y + (gridSize / 2)) / gridSize) * gridSize;
-//
-//		int xTile = (endX - startX) / gridSize; // number of times to tile horizontally
-//		int yTile = (endY - startY) / gridSize; // number of times to tile vertically
-
-//		for (int y = startY; y < endY; y += gridSize) {
-//			for (int x = startX; x < endX; x += gridSize) {
-//				
-//			}
-//		}
-
 		// find x start position
-		startX = (int) Math.round((topLeft.x - (gridSize / 2)) / gridSize);
+		float startX = (int) Math.round((topLeft.x - (gridSize / 2)) / gridSize);
 		// find y start position;
-		startY = (int) Math.round((topLeft.y - (gridSize / 2)) / gridSize);
+		float startY = (int) Math.round((topLeft.y - (gridSize / 2)) / gridSize);
 		// find x end position
-		endX = (int) Math.round((bottomRight.x + (gridSize / 2)) / gridSize);
+		float endX = (int) Math.round((bottomRight.x + (gridSize / 2)) / gridSize);
 		// find y end position
-		endY = (int) Math.round((bottomRight.y + (gridSize / 2)) / gridSize);
+		float endY = (int) Math.round((bottomRight.y + (gridSize / 2)) / gridSize);
 
-		float xTileStart = 0;
-		float yTileStart = 0;
-		float xTileEnd = endX - startX; // number of times to tile horizontally
-		float yTileEnd = endY - startY; // number of times to tile vertically
+		float xTileStart = 0; // where to start horizontal tiling in texture units
+		float yTileStart = 0; // where to start vertical tiling in texture units
+		float xTileEnd = endX - startX; // where to end horizontal tiling in texture units
+		float yTileEnd = endY - startY; // where to end vertical tiling in texture units
 
 		// convert to level dimensions
 		startX = startX * gridSize;
@@ -72,6 +39,7 @@ public class BackgroundPaper {
 		endX = endX * gridSize;
 		endY = endY * gridSize;
 
+		// these probably don't need to be if statements, they will always be true
 		if (startX < topLeft.x) {
 			// offset start by difference between startX and topLeft.x in texture units
 			xTileStart += (topLeft.x - startX) / gridSize;
@@ -85,10 +53,16 @@ public class BackgroundPaper {
 			startY = topLeft.y;
 		}
 		if (endX > bottomRight.x) {
-			// offset start by difference between endY and bottomRight.y in texture units
+			// offset start by difference between endX and bottomRight.x in texture units
 			xTileEnd -= (endX - bottomRight.x) / gridSize;
 			// move rectangle right edge over to screen end x
 			endX = bottomRight.x;
+		}
+		if (endY > bottomRight.y) {
+			// offset start by difference between endY and bottomRight.y in texture units
+			yTileEnd -= (endY - bottomRight.y) / gridSize;
+			// move rectangle right edge over to screen end y
+			endY = bottomRight.y;
 		}
 
 		// texture
@@ -103,109 +77,6 @@ public class BackgroundPaper {
 		graphics.vertex(startX, endY, xTileStart, yTileEnd); // bottom left
 		graphics.endShape();
 
-		// reference code
-//		if (x + gridSize > screen.getBottomRight().x) {
-//			rightEdge = screen.getBottomRight().x;
-//			imageEndX = (int) (image.width
-//					- (image.width * (((x + gridSize) - screen.getBottomRight().x) / gridSize)));
-//		}
-
-		// nested for loops to tile the images
-//		for (int y = startY; y < endY; y += gridSize) {
-//			for (int x = startX; x < endX; x += gridSize) {
-
-//				image = texture.getPageViewBackground(scale);
-//
-//				leftEdge = x;
-//				topEdge = y;
-//				rightEdge = x + gridSize;
-//				bottomEdge = y + gridSize;
-//
-//				imageStartX = 0;
-//				imageStartY = 0;
-//				imageEndX = image.width;
-//				imageEndY = image.height;
-//
-//				if (x + gridSize / 2 < topLeft.x) {
-//					leftEdge = leftEdge + gridSize / 2;
-//					imageStartX = image.width / 2;
-//				}
-//				if (y + gridSize / 2 < topLeft.y) {
-//					topEdge = topEdge + gridSize / 2;
-//					imageStartY = image.height / 2;
-//				}
-//				if (x + gridSize / 2 > bottomRight.x) {
-//					rightEdge = rightEdge - gridSize / 2;
-//					imageEndX = image.width / 2;
-//				}
-//				if (y + gridSize / 2 > bottomRight.y) {
-//					bottomEdge = bottomEdge - gridSize / 2;
-//					imageEndY = image.height / 2;
-//				}
-
-//				graphics.imageMode(CORNERS);
-//				graphics.image(image, leftEdge, topEdge, rightEdge, bottomEdge, imageStartX, imageStartY, imageEndX,
-//						imageEndY);
-
-//				graphics.imageMode(CORNER);
-//				graphics.image(texture.getPageViewBackground(scale), x, y, gridSize, gridSize);
-//
-
-//			}
-//		}
-
 	}
 
-//	public void draw(PGraphics graphics, Rectangle screen, float scale) {
-//		// find x start position
-//		startX = (int) Math.round((screen.getTopLeft().x - (gridSize / 2)) / gridSize) * gridSize;
-//		// find y start position
-//		startY = (int) Math.round((screen.getTopLeft().y - (gridSize / 2)) / gridSize) * gridSize;
-//		// find x end position
-//		endX = (int) Math.round((screen.getBottomRight().x + (gridSize / 2)) / gridSize) * gridSize;
-//		// find y end position
-//		endY = (int) Math.round((screen.getBottomRight().y + (gridSize / 2)) / gridSize) * gridSize;
-//
-//		// nested for loops to tile the images
-//		for (int y = startY; y < endY; y += gridSize) {
-//			for (int x = startX; x < endX; x += gridSize) {
-//
-//				image = texture.getPageViewBackground(scale);
-//
-//				leftEdge = x;
-//				topEdge = y;
-//				rightEdge = x + gridSize;
-//				bottomEdge = y + gridSize;
-//
-//				imageStartX = 0;
-//				imageStartY = 0;
-//				imageEndX = image.width;
-//				imageEndY = image.height;
-//
-//				if (x < screen.getTopLeft().x) {
-//					leftEdge = screen.getTopLeft().x;
-//					imageStartX = (int) (image.width * ((screen.getTopLeft().x - x) / gridSize));
-//				}
-//				if (y < screen.getTopLeft().y) {
-//					topEdge = screen.getTopLeft().y;
-//					imageStartY = (int) (image.height * ((screen.getTopLeft().y - y) / gridSize));
-//				}
-//				if (x + gridSize > screen.getBottomRight().x) {
-//					rightEdge = screen.getBottomRight().x;
-//					imageEndX = (int) (image.width
-//							- (image.width * (((x + gridSize) - screen.getBottomRight().x) / gridSize)));
-//				}
-//				if (y + gridSize > screen.getBottomRight().y) {
-//					bottomEdge = screen.getBottomRight().y;
-//					imageEndY = (int) (image.height
-//							- (image.height * (((y + gridSize) - screen.getBottomRight().y) / gridSize)));
-//				}
-//
-//				graphics.imageMode(CORNERS);
-//				graphics.image(image, leftEdge, topEdge, rightEdge, bottomEdge, imageStartX, imageStartY, imageEndX,
-//						imageEndY);
-//
-//			}
-//		}
-//	}
 }
