c106de03d33284ea5336e52faaff16903bba2ed0
robotdognz <robotdognz@gmail.com>
2022-02-07 14:44:16 +1300

    More of the same

--- AppLogic.java
+++ AppLogic.java
@@ -200,7 +200,7 @@ public class AppLogic {
 				if (diff.x <= 0) {
 					// move to off right edge of pageArea
 
-					offsetX -= playerAreaCenter.x - pageArea.getTopLeft().x - menuArea.getWidth() / 2;
+					offsetX -= playerAreaCenter.x - pageArea.getTopLeft().x + menuArea.getWidth() / 2;
 
 				} else {
 					// move to off left edge of pageArea
