60d2fa96571155ac4edef3c9214b35f76a65b45e
robotdognz <robotdognz@gmail.com>
2022-03-01 14:23:07 +1300

    Trying a bigger dead zone

--- Eclipse/Unplayed/src/editor/uibottom/EditorBottom.java
+++ Eclipse/Unplayed/src/editor/uibottom/EditorBottom.java
@@ -179,7 +179,7 @@ public class EditorBottom extends Toolbar {
 	@Override
 	public void onTap(float x, float y) {
 		// select object
-		if (y >= selectionArea.getY() + 30) { // the 30 acts as padding so clicking on widgets doesn't select stuff
+		if (y >= selectionArea.getY() + 100) { // the 100 acts as padding so clicking on widgets doesn't select stuff
 			editor.controller = new EditorControl(p, editor);
 			editor.eMode = editorMode.ADD;
 
