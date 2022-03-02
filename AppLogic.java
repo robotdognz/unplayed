0db614f4152a7a3609d6fddd0776650aa92bf23f
robotdognz <robotdognz@gmail.com>
2022-03-03 11:36:16 +1300

    Possible fix

--- AppLogic.java
+++ AppLogic.java
@@ -285,6 +285,20 @@ public class AppLogic {
 				}
 				p.popMatrix();
 
+				// draw game and menu on top of the above forced rendering, this is so that you
+				// don't see the forced rendering on screen when clicking on the continue button
+				// (which calls this method outside of the draw loop and therefore force draws on
+				// top of the last frame)
+				if ((editor != null && !editorToggle) || (editor != null && Editor.showPageView) || (editor == null)) {
+					game.draw(); // draw the game
+				}
+				if (menu != null) {
+					if (!Camera.getGame()) {
+						menu.draw();
+					}
+					menu.hover(lastTouch);
+				}
+
 				// prevent animation jump by skipping the next frame
 				skipNextFrame = true;
 
@@ -339,18 +353,6 @@ public class AppLogic {
 
 		if (skipNextFrame) {
 			skipNextFrame = false;
-			// draw the game
-			if ((editor != null && !editorToggle) || (editor != null && Editor.showPageView) || (editor == null)) {
-				game.draw(); // draw the game
-			}
-			// draw the menu
-			if (menu != null) {
-				if (!Camera.getGame()) {
-					menu.draw();
-				}
-				menu.hover(lastTouch);
-			}
-			return;
 		}
 
 		// touch screen
