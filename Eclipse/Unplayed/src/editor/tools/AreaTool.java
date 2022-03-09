97c29863544e3afc54a0af09a28189645754d4cf
robotdognz <robotdognz@gmail.com>
2022-03-10 10:05:58 +1300

    Adding more conditions to area tool corner box drawing

--- Eclipse/Unplayed/src/editor/tools/AreaTool.java
+++ Eclipse/Unplayed/src/editor/tools/AreaTool.java
@@ -171,7 +171,8 @@ public class AreaTool implements Tool {
 			p.rect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
 			p.rectMode(CORNER);
 		}
-		if (edit != null && editor.controller instanceof EditorControl && editor.eMode == editorMode.SELECT) {
+		if (edit != null && editor.selected != null && editor.controller instanceof EditorControl
+				&& editor.eMode == editorMode.SELECT) {
 			p.noStroke();
 			p.fill(255, 0, 0);
 			p.rectMode(CENTER);
